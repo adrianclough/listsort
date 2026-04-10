e4de615218fc299da9b30839244605c84d39f7f1
Adrian Clough <adrianclough@gmail.com>
2026-04-10 17:58:07 +0200

    Write sort.

--- src/sort.py
+++ src/sort.py
@@ -1,8 +1,8 @@
-from math import floor
 from models import Item, SortMode
 from interaction import compare
 
-def top_sort(arr: list[Item]):
+def top_sort(arr: list[Item]) -> list[Item]:
+    """Sort top N-1 entries of list exhaustively."""
     if len(arr) < 2: 
         return arr
     
@@ -11,18 +11,48 @@ def top_sort(arr: list[Item]):
     bottom_top = []
     bottom_bottom = []
 
-    pivot_index = floor(len(arr)/2)
+    pivot_index = len(arr)//2
 
     for entry in arr[:pivot_index]:
-        if compare(entry, arr[pivot_index], 0, SortMode.TOP):
+        if compare(entry, arr[pivot_index], False, SortMode.TOP):
             top_bottom.append(entry)
         else:
             bottom_bottom.append(entry)
 
     for entry in arr[pivot_index + 1:]:
-        if compare(entry, arr[pivot_index], 1, SortMode.TOP):
+        if compare(entry, arr[pivot_index], True, SortMode.TOP):
             top_top.append(entry)
         else:
             bottom_top.append(entry)
 
     return top_sort(bottom_bottom + bottom_top) + [arr[pivot_index]] + top_sort(top_bottom + top_top)
+
+def sort(top: list[Item], n: int = 10) -> tuple[list[Item], list[Item]]:
+    """Sort todo list according to Adrian's algorithm."""
+
+    if len(top) < n:
+        return top_sort(top), []
+    
+    else:
+        top_top = []
+        top_bottom = []
+        bottom_top = []
+        bottom_bottom = []
+
+        pivot_index = len(top)//2
+
+        for entry in top[:pivot_index]:
+            if compare(entry, top[pivot_index], False, SortMode.TOP):
+                top_bottom.append(entry)
+            else:
+                bottom_bottom.append(entry)
+
+        for entry in top[pivot_index + 1:]:
+            if compare(entry, top[pivot_index], True, SortMode.TOP):
+                top_top.append(entry)
+            else:
+                bottom_top.append(entry)
+        
+        recursed_top, recursed_bottom = sort(top_top + top_bottom, n)
+
+        return recursed_top, recursed_bottom + [top[pivot_index]] + bottom_top + bottom_bottom
